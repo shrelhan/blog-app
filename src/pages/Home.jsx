f49a172e336e6d78d77e00b2358eb2892c237d01
shrelhan <shrelhan.relhan@gmail.com>
2024-09-11 21:22:15 +0530

    minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,7 +33,7 @@ function Home() {
       <Container>
         <div className="flex flex-wrap">
           {posts.map((post) => (
-            <div key={post.$id} className="p-2">
+            <div key={post.$id} className="p-2 w-1/4">
               <PostCard {...post} />
             </div>
           ))}
